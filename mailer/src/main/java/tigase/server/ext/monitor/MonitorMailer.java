0c6bf84c8c0766b2d2b0457d3afa04ea99b6c1ba
Bartosz Malkowski <bmalkowski@tigase.pl>
2017-05-15 10:22:54 +0200

    Task #5380: Maven modules
    updated dependencies
    mailer moved to separated module as MailSender

--- mailer/src/main/java/tigase/server/ext/monitor/MonitorMailer.java
+++ mailer/src/main/java/tigase/server/ext/monitor/MonitorMailer.java
@@ -1,18 +1,8 @@
 package tigase.server.ext.monitor;
 
-import java.util.Map;
-import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import javax.mail.Message;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
-import tigase.disteventbus.EventBus;
-import tigase.disteventbus.EventHandler;
+import tigase.eventbus.EventBus;
+import tigase.eventbus.EventListener;
+import tigase.extras.mailer.Mailer;
 import tigase.kernel.beans.Bean;
 import tigase.kernel.beans.Initializable;
 import tigase.kernel.beans.Inject;
@@ -20,28 +10,35 @@ import tigase.monitor.MonitorComponent;
 import tigase.monitor.MonitorExtension;
 import tigase.xml.Element;
 
-@Bean(name = MonitorMailer.ID)
-public class MonitorMailer implements MonitorExtension, Initializable {
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-	public static final String ID = "monitor-mailer";
+@Bean(name = "monitor-mailer", active = true, exportable = true)
+public class MonitorMailer
+		implements MonitorExtension, Initializable {
+
 	private final static String REMOTE_EVENT_INDICATOR = "remote";
-	protected final Logger log = Logger.getLogger(this.getClass().getName());
+	private final Logger log = Logger.getLogger(this.getClass().getName());
 	@Inject
 	private MonitorComponent component;
 
-	@Inject(nullAllowed = false)
+	@Inject
 	private EventBus eventBus;
 
 	private String fromAddress;
-	private Session session;
+	@Inject
+	private Mailer mailSender;
 	private String toAddresses;
-	private String username;
-	private String password;
-	private final EventHandler handler = new EventHandler() {
-
+	private final EventListener<Element> handler = new EventListener<Element>() {
 		@Override
-		public void onEvent(String name, String xmlns, Element event) {
-			MonitorMailer.this.onEvent(name, xmlns, event);
+		public void onEvent(Element event) {
+			String eventFullName = ((Element) event).getName();
+			int i = eventFullName.lastIndexOf(".");
+			final String packageName = i >= 0 ? eventFullName.substring(0, i) : "";
+			final String eventName = eventFullName.substring(i + 1);
+
+			MonitorMailer.this.onEvent(eventName, packageName, event);
 		}
 	};
 
@@ -62,8 +59,9 @@ public class MonitorMailer implements MonitorExtension, Initializable {
 			log.warning("Problem on reading property '" + name + "'");
 		}
 
-		if (result == null)
+		if (result == null) {
 			throw new RuntimeException("Property '" + name + "' is not defined!");
+		}
 
 		return result;
 	}
@@ -73,8 +71,9 @@ public class MonitorMailer implements MonitorExtension, Initializable {
 	}
 
 	protected void onEvent(final String name, final String xmlns, final Element event) {
-		if (event.getAttributeStaticStr(REMOTE_EVENT_INDICATOR) != null)
+		if (event.getAttributeStaticStr(REMOTE_EVENT_INDICATOR) != null) {
 			return;
+		}
 
 		String subject = "Tigase Monitor Notification";
 
@@ -84,7 +83,7 @@ public class MonitorMailer implements MonitorExtension, Initializable {
 		sb.append("hostname: ").append(component.getDefHostName()).append('\n');
 
 		if ("SampleTaskEnabled".equals(name)) {
-			String t = event.getCData(new String[] { "SampleTaskEnabled", "message" });
+			String t = event.getCData(new String[]{"SampleTaskEnabled", "message"});
 			subject += " - " + t;
 			sb.append("This is notification from sample monitor task.").append('\n');
 			for (Element c : event.getChildren()) {
@@ -101,60 +100,18 @@ public class MonitorMailer implements MonitorExtension, Initializable {
 	}
 
 	private void sendMail(String messageSubject, String messageText) {
-		try {
-			log.fine("Sending mail: " + messageText);
-
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(fromAddress));
-
-			InternetAddress[] to = InternetAddress.parse(toAddresses);
-			message.setRecipients(Message.RecipientType.TO, to);
-			message.setSubject(messageSubject);
-			message.setText(messageText);
-
-			if (username == null || username.trim().isEmpty()) {
-				Transport.send(message, to);
-			} else {
-				Transport.send(message, to, username, password);
-			}
-		} catch (Exception e) {
-			log.log(Level.WARNING, "Can't send mail", e);
-		}
+		mailSender.sendMail(fromAddress, toAddresses, messageSubject, messageText);
 	}
 
 	@Override
 	public void setProperties(Map<String, Object> props) {
 		log.config("Configuring Monitor Mailer");
-		Properties sessionProperties = new Properties();
-
-		sessionProperties.put("mail.smtp.auth", "false");
-		sessionProperties.put("mail.smtp.starttls.enable", "true");
-		sessionProperties.put("mail.smtp.ssl.trust", "*");
-		sessionProperties.put("mail.smtps.ssl.trust", "*");
-
-		sessionProperties.put("mail.smtp.ssl.checkserveridentity", "false");
-		sessionProperties.put("mail.smtp.ssl.trust", "*");
-		sessionProperties.put("mail.smtp.starttls.checkserveridentity", "false");
-		sessionProperties.put("mail.smtp.starttls.trust", "*");
-
 		try {
-			sessionProperties.put("mail.smtp.host", getRequiredProp(props, "mailer-smtp-host"));
-			sessionProperties.put("mail.smtp.port", getRequiredProp(props, "mailer-smtp-port"));
-
-			this.username = (String) props.get("mailer-smtp-username");
-			log.log(Level.CONFIG, "Setting username: {0}", username);
-			this.password = (String) props.get("mailer-smtp-password");
-			if (password != null && !password.isEmpty()) {
-				log.log(Level.CONFIG, "Setting password: {0}", password.replaceAll(".", "*"));
-			}
-
 			this.fromAddress = getRequiredProp(props, "mailer-from-address");
 			log.log(Level.CONFIG, "Setting fromAddress: {0}", fromAddress);
 			this.toAddresses = getRequiredProp(props, "mailer-to-addresses");
 			log.log(Level.CONFIG, "Setting toAddresses: {0}", toAddresses);
 
-			this.session = Session.getInstance(sessionProperties);
-			log.log(Level.CONFIG, "Setting session: {0}", session);
 		} catch (RuntimeException e) {
 			log.warning(e.getMessage());
 			log.warning("Mailer is not started");
@@ -162,7 +119,7 @@ public class MonitorMailer implements MonitorExtension, Initializable {
 		}
 
 		log.config("Initializing Monitor Mailer");
-		eventBus.addHandler(null, MonitorComponent.EVENTS_XMLNS, handler);
+		eventBus.addListener("tigase.monitor.tasks", null, handler);
 	}
 
 }
